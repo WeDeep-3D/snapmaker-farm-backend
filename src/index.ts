faeaff0255421e429640aaea02b100bb8392454b
ParticleG <particle_g@outlook.com>
2026-01-11 17:52:37 +0800

    add Elysia plugins for CORS, static files, and Swagger; update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
-import { Elysia } from "elysia";
+import cors from "@elysiajs/cors";
+import staticPlugin from "@elysiajs/static";
+import swagger from "@elysiajs/swagger";
+import {env} from "@yolk-oss/elysia-env";
+import {Elysia} from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(cors())
+  .use(env({}))
+  .use(staticPlugin())
+  .use(swagger())
+  .get("/", () => "Hello Elysia").listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
