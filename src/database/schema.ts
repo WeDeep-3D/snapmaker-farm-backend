7151f997a5b459a74f0920f70bc5465711a9c565
ParticleG <particle_g@outlook.com>
2026-01-12 17:38:52 +0800

    update project and database schema; add new device and filament fields; modify migration scripts

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,19 +1,21 @@
+import { sql } from 'drizzle-orm'
 import {
-  pgTable,
+  boolean,
+  char,
+  check,
   index,
-  uuid,
+  inet,
+  integer,
+  jsonb,
+  pgEnum,
+  pgTable,
+  primaryKey,
+  real,
   text,
   timestamp,
-  integer,
-  real,
-  check,
-  char,
-  boolean,
-  jsonb,
-  primaryKey,
-  pgEnum,
+  uniqueIndex,
+  uuid,
 } from 'drizzle-orm/pg-core'
-import { sql } from 'drizzle-orm'
 
 export const deviceModel = pgEnum('device_model', ['Snapmaker:U1'])
 export const filamentSubtype = pgEnum('filament_subtype', [
@@ -67,25 +69,79 @@ export const filamentVendor = pgEnum('filament_vendor', [
   'Snapmaker',
 ])
 
-export const projects = pgTable(
-  'projects',
+export const devices = pgTable(
+  'devices',
   {
+    // Common fields
     id: uuid()
       .default(sql`uuidv7()`)
       .primaryKey()
       .notNull(),
-    name: text(),
     description: text(),
     createdAt: timestamp('created_at', { mode: 'string' }).defaultNow(),
     updatedAt: timestamp('updated_at', { mode: 'string' }).defaultNow(),
-    startTime: timestamp('start_time', { mode: 'string' }).defaultNow(),
-    deliveryTime: timestamp('delivery_time', { mode: 'string' }),
+    // Identity fields
+    model: deviceModel().notNull(),
+    serialNumber: text('serial_number').notNull(),
+    // Network fields
+    ethIp: inet('eth_ip'),
+    ethMac: char('eth_mac', { length: 17 }),
+    wlanIp: inet('wlan_ip'),
+    wlanMac: char('wlan_mac', { length: 17 }),
+    // Relation fields
+    projectId: uuid('project_id'),
+    plateId: uuid('plate_id'),
   },
   (table) => [
-    index('idx_projects_id').using(
+    index('idx_devices_id').using(
       'btree',
       table.id.asc().nullsLast().op('uuid_ops'),
     ),
+    index('idx_devices_plate_id').using(
+      'btree',
+      table.plateId.asc().nullsLast().op('uuid_ops'),
+    ),
+    uniqueIndex('uq_devices_model_serial').on(table.model, table.serialNumber),
+  ],
+)
+export const filaments = pgTable(
+  'filaments',
+  {
+    // Common
+    id: uuid()
+      .default(sql`uuidv7()`)
+      .primaryKey()
+      .notNull(),
+    description: text(),
+    createdAt: timestamp('created_at', { mode: 'string' }).defaultNow(),
+    updatedAt: timestamp('updated_at', { mode: 'string' }).defaultNow(),
+
+    name: text(),
+    count: integer().default(1),
+    vendor: filamentVendor().default('Generic'),
+    type: filamentType().notNull(),
+    subtype: filamentSubtype(),
+    color: char({ length: 8 }).default('FFFFFFFF'),
+    density: real().notNull(),
+    diameter: real().notNull(),
+    isSoluble: boolean('is_soluble').default(false),
+    isSupport: boolean('is_support').default(false),
+    shrinkFactorHorizontal: real('shrink_factor_horizontal').default(1),
+    shrinkFactorVertical: real('shrink_factor_vertical').default(1),
+    temperatureIdle: real('temperature_idle').default(0),
+    temperatureSoftening: real('temperature_softening'),
+    defaultConfig: jsonb('default_config').default({}),
+    overrideConfig: jsonb('override_config').default({}),
+  },
+  (table) => [
+    index('idx_filaments_id').using(
+      'btree',
+      table.id.asc().nullsLast().op('uuid_ops'),
+    ),
+    check('count_positive_check', sql`count > 0`),
+    check('color_rgba_hex_check', sql`color ~ '^#[0-9a-fA-F]{8}$'::text`),
+    check('density_positive_check', sql`density > (0.0)::double precision`),
+    check('diameter_positive_check', sql`diameter > (0.0)::double precision`),
   ],
 )
 
@@ -120,72 +176,6 @@ export const plates = pgTable(
   ],
 )
 
-export const devices = pgTable(
-  'devices',
-  {
-    id: uuid()
-      .default(sql`uuidv7()`)
-      .primaryKey()
-      .notNull(),
-    name: text(),
-    description: text(),
-    createdAt: timestamp('created_at', { mode: 'string' }).defaultNow(),
-    updatedAt: timestamp('updated_at', { mode: 'string' }).defaultNow(),
-    model: deviceModel().notNull(),
-    projectId: uuid('project_id'),
-    plateId: uuid('plate_id'),
-  },
-  (table) => [
-    index('idx_devices_id').using(
-      'btree',
-      table.id.asc().nullsLast().op('uuid_ops'),
-    ),
-    index('idx_devices_plate_id').using(
-      'btree',
-      table.plateId.asc().nullsLast().op('uuid_ops'),
-    ),
-  ],
-)
-
-export const filaments = pgTable(
-  'filaments',
-  {
-    id: uuid()
-      .default(sql`uuidv7()`)
-      .primaryKey()
-      .notNull(),
-    name: text(),
-    description: text(),
-    createdAt: timestamp('created_at', { mode: 'string' }).defaultNow(),
-    updatedAt: timestamp('updated_at', { mode: 'string' }).defaultNow(),
-    count: integer().default(1),
-    vendor: filamentVendor().default('Generic'),
-    type: filamentType().notNull(),
-    subtype: filamentSubtype(),
-    color: char({ length: 8 }).default('FFFFFFFF'),
-    density: real().notNull(),
-    diameter: real().notNull(),
-    isSoluble: boolean('is_soluble').default(false),
-    isSupport: boolean('is_support').default(false),
-    shrinkFactorHorizontal: real('shrink_factor_horizontal').default(1),
-    shrinkFactorVertical: real('shrink_factor_vertical').default(1),
-    temperatureIdle: real('temperature_idle').default(0),
-    temperatureSoftening: real('temperature_softening'),
-    defaultConfig: jsonb('default_config').default({}),
-    overrideConfig: jsonb('override_config').default({}),
-  },
-  (table) => [
-    index('idx_filaments_id').using(
-      'btree',
-      table.id.asc().nullsLast().op('uuid_ops'),
-    ),
-    check('count_positive_check', sql`count > 0`),
-    check('color_rgba_hex_check', sql`color ~ '^#[0-9a-fA-F]{8}$'::text`),
-    check('density_positive_check', sql`density > (0.0)::double precision`),
-    check('diameter_positive_check', sql`diameter > (0.0)::double precision`),
-  ],
-)
-
 export const plateRelatedFilaments = pgTable(
   'plate_related_filaments',
   {
@@ -205,5 +195,27 @@ export const plateRelatedFilaments = pgTable(
       columns: [table.plateId, table.filamentId],
       name: 'plate_related_filaments_pkey',
     }),
+  ],
+)
+
+export const projects = pgTable(
+  'projects',
+  {
+    id: uuid()
+      .default(sql`uuidv7()`)
+      .primaryKey()
+      .notNull(),
+    name: text(),
+    description: text(),
+    createdAt: timestamp('created_at', { mode: 'string' }).defaultNow(),
+    updatedAt: timestamp('updated_at', { mode: 'string' }).defaultNow(),
+    startTime: timestamp('start_time', { mode: 'string' }).defaultNow(),
+    deliveryTime: timestamp('delivery_time', { mode: 'string' }),
+  },
+  (table) => [
+    index('idx_projects_id').using(
+      'btree',
+      table.id.asc().nullsLast().op('uuid_ops'),
+    ),
   ],
 )
