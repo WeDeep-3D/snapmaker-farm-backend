30597b52f3c1d351aac5f5e5348903580f12e5fa
ParticleG <particle_g@outlook.com>
2026-02-05 16:50:13 +0800

    feat: add endpoint to retrieve and zip logs for a specific device

--- src/modules/devices/index.ts
+++ src/modules/devices/index.ts
@@ -1,7 +1,10 @@
+import axios from 'axios'
 import { Elysia } from 'elysia'
 
 import { devicesModel } from './model'
 import { devicesService } from './service'
+import { packToZip } from '@/utils/io'
+import { buildErrorResponse, buildSuccessResponse } from '@/utils/common'
 
 export const devices = new Elysia({
   prefix: '/api/v1/devices',
@@ -16,3 +19,48 @@ export const devices = new Elysia({
       unknown: store.unknownDevices,
     },
   }))
+  .get('/:ip/logs', async ({ params }) => {
+    // noinspection HttpUrlsUsage
+    const filesApi = axios.create({
+      baseURL: `http://${params.ip}:7125/server/files`,
+    })
+    try {
+      const fileList = await Promise.all(
+        (
+          await filesApi.get<{
+            result: { path: string; modified: number; size: number; permissions: string }[]
+          }>(`/list?root=logs`)
+        ).data.result.map(async (fileData) => {
+          try {
+            return new File(
+              [
+                new Blob([(await axios.get<ArrayBuffer>(`/logs/${fileData.path}`)).data], {
+                  type: 'application/octet-stream',
+                }),
+              ],
+              fileData.path,
+              {
+                lastModified: fileData.modified,
+                type: 'application/octet-stream',
+              },
+            )
+          } catch (error) {
+            return new File(
+              [new Blob([(error as Error).message], { type: 'text/plain' })],
+              fileData.path,
+              {
+                lastModified: fileData.modified,
+                type: 'application/octet-stream',
+              },
+            )
+          }
+        }),
+      )
+      const blob = await packToZip(fileList)
+      return buildSuccessResponse(
+        new File([blob], `logs_${params.ip}.zip`, { type: 'application/zip' }),
+      )
+    } catch (error) {
+      return buildErrorResponse(500, (error as Error).message)
+    }
+  })
